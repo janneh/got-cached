11003d637dc06ac22ea995aa13c7d1462eedbf35
Janne Lönn <janne@izettle.com>
2016-04-23 21:43:50 +0200

    Handle caching json

--- index.js
+++ index.js
@@ -4,13 +4,16 @@ import got from 'got'
 * Returns `got` wrapped with cache to be used as normal got
 */
 
-export default function gotCached(options) {
-  if (!options.cache) throw Error('cache is a required option')
-  const { cache } = options
+export default function gotCached(config) {
+  if (!config.cache) throw Error('cache is a required option')
+  const { cache } = config
 
-  function setCache(key, response) {
+  function setCache(key, response, options) {
     const promise = response.then(response => {
-      cache.set(key, response.body)
+      const value = options.json
+        ? JSON.stringify(response.body)
+        : response.body
+      cache.set(key, value)
 
       return Promise.resolve(response)
     })
@@ -18,9 +21,10 @@ export default function gotCached(options) {
     return promise
   }
 
-  function getCache(key) {
+  function getCache(key, options) {
     const promise = cache.get(key).then(value => {
       if (!value) return null
+      if (options.json) value = JSON.parse(value)
 
       return Promise.resolve({
         status: 200,
@@ -37,21 +41,17 @@ export default function gotCached(options) {
       return got(url, options)
     }
 
-    const cachedResponse = getCache(url)
+    const cachedResponse = getCache(url, options)
       .then((cached) => {
         // return the cached result if it exist
         if(cached) return cached
 
         // return got response but first set cache with it
-        return setCache(url, got(url, options))
+        return setCache(url, got(url, options), options)
       })
 
     return cachedResponse
   }
 
-  for(const prop in got) {
-    cachedGot[prop] = got[prop]
-  }
-
-  return cachedGot
+  return Object.assign(cachedGot, got)
 }
